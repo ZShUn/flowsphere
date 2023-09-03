1793bd44bb197c808460702f2a2110a23b2ca4c2
ZShUn <616394599@qq.com>
2023-09-04 07:30:02 +0800

    update MethodMatcherConfigCreator

--- ancient-agent-core/src/main/java/com/ancient/agent/core/config/MethodMatcherConfigCreator.java
+++ ancient-agent-core/src/main/java/com/ancient/agent/core/config/MethodMatcherConfigCreator.java
@@ -1,13 +1,34 @@
 package com.ancient.agent.core.config;
 
 import com.ancient.agent.core.config.yaml.YamlMethodPointcutConfig;
+import com.ancient.agent.core.enums.MethodTypeEnum;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+import net.bytebuddy.matcher.ElementMatchers;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class MethodMatcherConfigCreator {
 
     public static List<MethodMatcherConfig> create(Collection<YamlMethodPointcutConfig> yamlMethodPointcutConfigs) {
+        List<MethodMatcherConfig> result = new ArrayList<>(yamlMethodPointcutConfigs.size());
+        for (YamlMethodPointcutConfig yamlConfig : yamlMethodPointcutConfigs) {
+            result.add(new MethodMatcherConfig(createPointcut(yamlConfig),
+                    yamlConfig.getInterceptorName(), yamlConfig.getType()));
+        }
+        return result;
+    }
+
+
+    private static ElementMatcher<? super MethodDescription> createPointcut(YamlMethodPointcutConfig yamlConfig) {
+        if (MethodTypeEnum.INSTANT.getType().equals(yamlConfig.getType())) {
+            return ElementMatchers.named(yamlConfig.getMethodName());
+        }
+        if (MethodTypeEnum.CONSTRUCTOR.getType().equals(yamlConfig.getType())) {
+            return ElementMatchers.isConstructor();
+        }
         return null;
     }
 
