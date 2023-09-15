74861c462970783081fc69b3f9c3f9dd5f2b524b
ZShUn <616394599@qq.com>
2023-09-15 22:03:43 +0800

    update nacos-plugin update rocketmq、springmvc、feign、springcloudgateway yaml file

--- ancient-plugins/nacos/src/main/java/com/ancient/plugin/nacos/NacosInstantMethodInterceptor.java
+++ ancient-plugins/nacos/src/main/java/com/ancient/plugin/nacos/NacosInstantMethodInterceptor.java
@@ -1,11 +1,18 @@
 package com.ancient.plugin.nacos;
 
+import com.alibaba.cloud.nacos.ribbon.NacosServer;
 import com.ancient.agent.core.context.CustomContextAccessor;
-import com.ancient.agent.core.interceptor.type.InstantMethodInterceptor;
 import com.ancient.agent.core.interceptor.template.InstantMethodInterceptorResult;
-import com.ancient.plugin.nacos.enhance.NacosServerEnhance;
+import com.ancient.agent.core.interceptor.type.InstantMethodInterceptor;
+import com.ancient.common.constant.CommonConstant;
+import com.ancient.common.context.RuleContext;
+import com.ancient.common.entity.RuleEntity;
+import com.netflix.loadbalancer.Server;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.Callable;
 
 public class NacosInstantMethodInterceptor implements InstantMethodInterceptor {
@@ -17,8 +24,24 @@ public class NacosInstantMethodInterceptor implements InstantMethodInterceptor {
 
     @Override
     public void afterMethod(CustomContextAccessor customContextAccessor, Object[] allArguments, Callable<?> callable, Method method, Object result) {
-        NacosServerEnhance.getInstance().enhance(customContextAccessor, allArguments, callable, method, result, null);
-
+        if (result instanceof List) {
+            List<Server> serverList = (List<Server>) result;
+            serverList = new ArrayList<>(serverList);
+            for (int i = serverList.size() - 1; i >= 0; i--) {
+                Server server = serverList.get(i);
+                if (server instanceof NacosServer) {
+                    NacosServer nacosServer = (NacosServer) server;
+                    String version = nacosServer.getInstance().getMetadata().get(CommonConstant.VERSION);
+                    RuleEntity ruleEntity = RuleContext.get();
+                    if (Objects.nonNull(ruleEntity)) {
+                        if (Objects.nonNull(ruleEntity.getVersionEntity()) &&
+                                !ruleEntity.getVersionEntity().getVersion().equals(version)) {
+                            serverList.remove(server);
+                        }
+                    }
+                }
+            }
+        }
     }
 
 
