d2a218d37a7cdd4a86f4aed9b860a6dd9d18e335
ZShUn <616394599@qq.com>
2023-09-18 07:52:19 +0800

    update MultiThreadMethodInterceptorBuilder

--- ancient-agent-core/src/main/java/com/ancient/agent/core/builder/MultiThreadMethodInterceptorBuilder.java
+++ ancient-agent-core/src/main/java/com/ancient/agent/core/builder/MultiThreadMethodInterceptorBuilder.java
@@ -4,12 +4,17 @@ import com.ancient.agent.core.interceptor.MultiThreadMethodInterceptor;
 import com.ancient.agent.core.matcher.MultiThreadMethodMatch;
 import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.implementation.SuperMethodCall;
+import net.bytebuddy.matcher.ElementMatchers;
 
 public class MultiThreadMethodInterceptorBuilder implements InterceptorBuilder {
 
     @Override
     public DynamicType.Builder<?> intercept(DynamicType.Builder<?> builder) {
-        builder.method(new MultiThreadMethodMatch()).intercept(MethodDelegation.to(MultiThreadMethodInterceptor.class));
+        builder.method(new MultiThreadMethodMatch())
+                .intercept(MethodDelegation.to(MultiThreadMethodInterceptor.class))
+                .constructor(ElementMatchers.isConstructor())
+                .intercept(SuperMethodCall.INSTANCE.andThen(MethodDelegation.withDefaultConfiguration().to(MultiThreadMethodInterceptor.class)));
         return builder;
     }
 
