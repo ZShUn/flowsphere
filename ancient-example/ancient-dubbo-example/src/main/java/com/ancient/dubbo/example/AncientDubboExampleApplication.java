ae81332635fa5573315a06064ec60be597cd8359
ZShUn <616394599@qq.com>
2023-09-21 07:47:47 +0800

    add ancient-dubbo-example

---
+++ ancient-example/ancient-dubbo-example/src/main/java/com/ancient/dubbo/example/AncientDubboExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ancient.dubbo.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncientDubboExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AncientDubboExampleApplication.class, args);
+    }
+
+}
---
+++ ancient-example/ancient-dubbo-example/src/test/java/com/ancient/dubbo/example/AncientDubboExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ancient.dubbo.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AncientDubboExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
