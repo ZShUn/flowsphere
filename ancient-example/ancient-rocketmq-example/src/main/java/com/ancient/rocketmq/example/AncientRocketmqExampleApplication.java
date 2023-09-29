8aa7cf15765c4322f9164d6d82795fb679bc7f29
ZShUn <616394599@qq.com>
2023-09-29 20:31:09 +0800

    add rocketmq example

---
+++ ancient-example/ancient-rocketmq-example/src/main/java/com/ancient/rocketmq/example/AncientRocketmqExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ancient.rocketmq.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncientRocketmqExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AncientRocketmqExampleApplication.class, args);
+    }
+
+}
