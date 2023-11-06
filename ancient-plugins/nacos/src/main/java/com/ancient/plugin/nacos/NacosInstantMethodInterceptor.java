2a56508862e3ca3546b07ee8d4e195a7f5179c33
ZShUn <616394599@qq.com>
2023-11-07 07:40:04 +0800

    add nacos guarantee router

--- ancient-plugins/nacos/src/main/java/com/ancient/plugin/nacos/NacosInstantMethodInterceptor.java
+++ ancient-plugins/nacos/src/main/java/com/ancient/plugin/nacos/NacosInstantMethodInterceptor.java
@@ -8,6 +8,7 @@ import com.ancient.common.constant.CommonConstant;
 import com.ancient.common.rule.context.TagContext;
 import com.google.common.base.Strings;
 import com.netflix.loadbalancer.Server;
+import org.springframework.util.CollectionUtils;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -18,23 +19,28 @@ public class NacosInstantMethodInterceptor implements InstantMethodInterceptor {
 
     @Override
     public void beforeMethod(CustomContextAccessor customContextAccessor, Object[] allArguments, Callable<?> callable, Method method, InstantMethodInterceptorResult instantMethodInterceptorResult) {
-        Object result = allArguments[0];
-        if (result instanceof List) {
-            List<Server> serverList = (List<Server>) result;
-            serverList = new ArrayList<>(serverList);
-            for (int i = serverList.size() - 1; i >= 0; i--) {
-                Server server = serverList.get(i);
+        Object serverListObj = allArguments[0];
+        if (serverListObj instanceof List) {
+            List<Server> servers = (List<Server>) serverListObj;
+            List<Server> result = new ArrayList<>(servers);
+            for (int i = result.size() - 1; i >= 0; i--) {
+                Server server = result.get(i);
                 if (server instanceof NacosServer) {
                     NacosServer nacosServer = (NacosServer) server;
                     String serverTag = nacosServer.getInstance().getMetadata().get(CommonConstant.TAG);
                     String ruleTag = TagContext.get();
                     if (!Strings.isNullOrEmpty(ruleTag) && !ruleTag.equals(serverTag) && !Strings.isNullOrEmpty(serverTag)) {
-                        serverList.remove(server);
+                        result.remove(server);
                     }
                 }
             }
             instantMethodInterceptorResult.setContinue(false);
-            instantMethodInterceptorResult.setResult(serverList);
+            //兜底路由
+            if (CollectionUtils.isEmpty(result)) {
+                instantMethodInterceptorResult.setResult(servers);
+                return;
+            }
+            instantMethodInterceptorResult.setResult(result);
 
         }
     }
